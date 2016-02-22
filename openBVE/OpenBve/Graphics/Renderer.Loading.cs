7cccade8df424a30afdfea9f64c2079b35efb8b6
Maurizio M. Gavioli <mmg@vistamaresoft.com>
2016-02-22 13:57:45 +0100

    Route-train loading screen
    Improves (hopefully!) the loading screen by:
    
    - reducing the logo texture to a less intruding size, which also fits any reasonable screen size while remaining at power of 2 w/h without scaling for a better rendering quality;
    - displaying actual assembly version;
    - displaying current web site URL (correct?)
    - displaying a progress bar (cumulative for route and train)
    
    An SVG drawing for creating textures of appropriate sizes is included.

--- openBVE/OpenBve/Graphics/Renderer.Loading.cs
+++ openBVE/OpenBve/Graphics/Renderer.Loading.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Reflection;			// for AssemblyVersion
 using OpenBveApi.Colors;
 using OpenTK.Graphics.OpenGL;
 
@@ -9,6 +10,10 @@ namespace OpenBve {
 		/* --------------------------------------------------------------
 		 * This file contains the drawing routines for the loading screen
 		 * -------------------------------------------------------------- */
+
+		private static Color128	ColourBackground	= new Color128 (0.39f, 0.39f, 0.39f, 1.0f);
+		private const int		progrBorder			= 1;
+		private const int		progrMargin			= 24;
 
 		internal static void DrawLoadingScreen() {
             
@@ -22,27 +27,53 @@ namespace OpenBve {
 				GL.Disable(EnableCap.Lighting);
 				LightingEnabled = false;
 			}
-			int size = Math.Min(Screen.Width, Screen.Height);
 			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 			GL.PushMatrix();
-				DrawRectangle(TextureLogo, new Point((Screen.Width - size) / 2, (Screen.Height - size) / 2), new Size(size, size), Color128.White);
-			DrawRectangle(null, 
-				new Point((Screen.Width - size) / 2, Screen.Height - (int)Fonts.NormalFont.FontSize - 10),
-				new Size(Screen.Width, (int)Fonts.NormalFont.FontSize + 10), 
-				new Color128(0.0f, 0.0f, 0.0f, 0.5f));
-			double routeProgress = Math.Max(0.0, Math.Min(1.0, Loading.RouteProgress));
-			double trainProgress = Math.Max(0.0, Math.Min(1.0, Loading.TrainProgress));
-			string text;
-			if (routeProgress < 1.0) {
-				string percent = (100.0 * routeProgress).ToString("0.0");
-				text = String.Format("{0} {1}%",Interface.GetInterfaceString("loading_loading_route"),percent);
-			} else if (trainProgress < 1.0) {
-				string percent = (100.0 * trainProgress).ToString("0.0");
-				text = String.Format("{0} {1}%",Interface.GetInterfaceString("loading_loading_train"),percent);
-			} else {
-				text = Interface.GetInterfaceString("message_loading");
-			}
-			DrawString(Fonts.SmallFont, text, new Point((Screen.Width - size) / 2 + 5, Screen.Height - (int)(Fonts.NormalFont.FontSize / 2) - 5), TextAlignment.CenterLeft, Color128.White);
+			// place the centre of the logo at the golden ratio of the screen height
+			int logoTop		= (int)(Screen.Height * 0.381966 - TextureLogo.Height / 2);
+			int	logoBottom	= logoTop + TextureLogo.Height;
+			// draw a dark gray background and the (unscaled) logo bitmap centred in the screen
+			DrawRectangle(null, new Point(0, 0), new Size(Screen.Width, Screen.Height), ColourBackground);
+			DrawRectangle(TextureLogo,
+				new Point((Screen.Width - TextureLogo.Width) / 2, logoTop),
+				new Size(TextureLogo.Width, TextureLogo.Height), Color128.White);
+			if (logoTop == logoBottom)			// if logo texture not yet loaded, do nothing else
+				return;
+			// take the height remaining below the logo and divide in 3 horiz. parts
+			int	fontHeight	= (int)Fonts.SmallFont.FontSize;
+			int	blankHeight	= (Screen.Height - logoBottom) / 3;
+			int	versionTop	= logoBottom + blankHeight - fontHeight;
+			int	halfWidth	= Screen.Width/2;
+			// draw version number and web site URL
+			DrawString(Fonts.SmallFont, "Version " + typeof(Renderer).Assembly.GetName().Version,
+				new Point(halfWidth, versionTop), TextAlignment.TopMiddle, Color128.White);
+			// for the moment, do not show any URL
+//			DrawString(Fonts.SmallFont, "https://sites.google.com/site/openbvesim/home",
+//				new Point(halfWidth, versionTop + fontHeight+2),
+//				TextAlignment.TopMiddle, Color128.White);
+			// draw progress message and bar
+			int		progressTop		= Screen.Height - blankHeight;
+			int		progressWidth	= Screen.Width - progrMargin * 2;
+			double	routeProgress	= Math.Max(0.0, Math.Min(1.0, Loading.RouteProgress));
+			double	trainProgress	= Math.Max(0.0, Math.Min(1.0, Loading.TrainProgress));
+			string	text			= Interface.GetInterfaceString(
+				routeProgress < 1.0 ? "loading_loading_route" :
+				(trainProgress < 1.0 ? "loading_loading_train" : "message_loading") );
+			DrawString(Fonts.SmallFont, text, new Point(halfWidth, progressTop - fontHeight - 6),
+				TextAlignment.TopMiddle, Color128.White);
+			// sum of route progress and train progress arrives up to 2.0:
+			// => 50.0 * to convert to %
+			double	percent	= 50.0 * (routeProgress + trainProgress);
+			string	percStr	= percent.ToString("0") + "%";
+			// progress frame
+			DrawRectangle(null, new Point(progrMargin-progrBorder, progressTop-progrBorder),
+				new Size(progressWidth+progrBorder*2, fontHeight+6), Color128.White);
+			// progress bar
+			DrawRectangle(null, new Point(progrMargin, progressTop),
+				new Size(progressWidth * (int)percent / 100, fontHeight+4), Color128.Cyan);
+			// progress percent
+			DrawString(Fonts.SmallFont, percStr, new Point(halfWidth, progressTop),
+				TextAlignment.TopMiddle, Color128.Black);
 			GL.PopMatrix();             
 		}
 		
