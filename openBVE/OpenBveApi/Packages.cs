595f6476a0da6bcaa4d9a2e00e4c8614df9739ca
leezer3 <leezer3@gmail.com>
2016-02-26 12:51:02 +0000

    Move package definition to the OpenBveApi

---
+++ openBVE/OpenBveApi/Packages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OpenBveApi.Packages
+{
+    /// <summary>Defines an OpenBVE Package</summary>
+    /// This is an arbritrarily formatted archive containing the following files:
+    /// package.xml - Package descriptor
+    /// package.png - Package image
+    /// package.rtf - Rich-text package description
+    public class Package
+    {
+        /// <summary>The package version</summary>
+        public Version PackageVersion;
+        /// <summary>The package name</summary>
+        public string Name = "";
+        /// <summary>The package author</summary>
+        public string Author = "";
+        /// <summary>The package website</summary>
+        public string Website = "";
+        /// <summary>The GUID for this package</summary>
+        public string GUID = "";
+        /// <summary>Stores the package type- 0 for routes and 1 for trains</summary>
+        public int PackageType;
+
+        /*
+         * These values are used by dependancies
+         * They need to live in the base Package class to save creating another.....
+         */
+        /// <summary>The minimum package version</summary>
+        public Version MinimumVersion;
+        /// <summary>The maximum package version</summary>
+        public Version MaximumVersion;
+    }
+}
