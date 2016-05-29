1aa1cf37168942420e74d9dd37843a3870a34af5
Christopher Lees <leezer3@gmail.com>
2016-05-29 23:45:08 +0100

    Fix: Handle case where the Object, Route and Sound folders are not in a Railway folder.

--- RouteViewer/LoadingR.cs
+++ RouteViewer/LoadingR.cs
@@ -58,6 +58,25 @@ namespace OpenBve {
 					Folder = Info.FullName;
 				}
 			} catch { }
+			//If the Route, Object and Sound folders exist, but are not in a railway folder.....
+			try
+			{
+				string Folder = System.IO.Path.GetDirectoryName(RouteFile);
+				while (true)
+				{
+					string RouteFolder = OpenBveApi.Path.CombineDirectory(Folder, "Route");
+					string ObjectFolder = OpenBveApi.Path.CombineDirectory(Folder, "Object"); 
+					string SoundFolder = OpenBveApi.Path.CombineDirectory(Folder, "Sound");
+					if (System.IO.Directory.Exists(RouteFolder) && System.IO.Directory.Exists(ObjectFolder) && System.IO.Directory.Exists(SoundFolder))
+					{
+						return Folder;
+					}
+					System.IO.DirectoryInfo Info = System.IO.Directory.GetParent(Folder);
+					if (Info == null) break;
+					Folder = Info.FullName;
+				}
+			}
+			catch { }
 			return Application.StartupPath;
 		}
 
