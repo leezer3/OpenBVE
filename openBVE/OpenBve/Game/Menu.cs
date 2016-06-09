e938325aebfc1075c2ad38846e133b36127261b6
Christopher Lees <leezer3@gmail.com>
2016-06-09 01:07:12 +0100

    Compiler warnings:
    * Add XML descriptions for some of the menu stuff.
    [ci skip]

--- openBVE/OpenBve/Game/Menu.cs
+++ openBVE/OpenBve/Game/Menu.cs
@@ -17,8 +17,31 @@ namespace OpenBve
 	public sealed class Menu
 	{
 		/// <summary>The list of possible tags for a menu entry- These define the functionality of a given menu entry</summary>
-		public enum MenuTag		{ None, Caption, MenuBack, MenuJumpToStation, MenuExitToMainMenu, MenuControls, MenuQuit,
-			BackToSim, JumpToStation, ExitToMainMenu, Quit, Control };
+		public enum MenuTag		{ 
+			/// <summary>Has no functionality/ is blank</summary>
+			None,
+			/// <summary>Is a caption for another menu item</summary>
+			Caption,
+			/// <summary>Moves up a menu level</summary>
+			MenuBack,
+			/// <summary>Enters the submenu containing the list of stations to which the player train may be jumped</summary>
+			MenuJumpToStation,
+			/// <summary>Enters the submenu for exiting to the main menu</summary>
+			MenuExitToMainMenu,
+			/// <summary>Enters the submenu for customising controls</summary>
+			MenuControls,
+			/// <summary>Enters the submenu for quitting the program</summary>
+			MenuQuit,
+			/// <summary>Returns to the simulation</summary>
+			BackToSim,
+			/// <summary>Jumps to the selected station</summary>
+			JumpToStation,
+			/// <summary>Exits to the main menu</summary>
+			ExitToMainMenu,
+			/// <summary>Quits the program</summary>
+			Quit,
+			/// <summary>Customises the selected control</summary>
+			Control };
 
 		/// <summary>The list of possible sub-menu types</summary>
 		public enum MenuType	{ None, Top, JumpToStation, ExitToMainMenu, Controls, Control, Quit };
