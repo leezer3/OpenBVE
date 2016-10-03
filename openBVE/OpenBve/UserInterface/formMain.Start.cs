leezer3/OpenBVE

commit 6b78dba6b48b80b115e0343db6b7af30a77fd90c

Fix: A 'Train' folder within the 'Railway' folder caused train searching to break.

openBVE/OpenBve/UserInterface/formMain.Start.cs
@@ -896,21 +896,38 @@ namespace OpenBve {
 			try {
 				while (true) {
 					string TrainFolder = OpenBveApi.Path.CombineDirectory(Folder, "Train");
+					var OldFolder = Folder;
 					if (System.IO.Directory.Exists(TrainFolder)) {
 						try {
 							Folder = OpenBveApi.Path.CombineDirectory(TrainFolder, Game.TrainName);
 						} catch {
 							Folder = null;
 						}
-						if (Folder != null && System.IO.Directory.Exists(Folder)) {
-							string File = OpenBveApi.Path.CombineFile(Folder, "train.dat");
-							if (System.IO.File.Exists(File)) {
-								// train found
-								Result.TrainFolder = Folder;
-								ShowTrain(false);
-								return;
+						if (Folder != null) {
+							if (System.IO.Directory.Exists(Folder))
+							{
+
+								string File = OpenBveApi.Path.CombineFile(Folder, "train.dat");
+								if (System.IO.File.Exists(File))
+								{
+									// train found
+									Result.TrainFolder = Folder;
+									ShowTrain(false);
+									return;
+								}
 							}
-						} break;
+							else if (Folder.ToLowerInvariant().Contains("\\railway\\"))
+							{
+								//If we have a misplaced Train folder in either our Railway\Route
+								//or Railway folders, this can cause the train search to fail
+								//Detect the presence of a railway folder and carry on traversing upwards if this is the case
+								Folder = OldFolder;
+							}
+							else
+							{
+								break;
+							}
+						}
 					}
 					if (Folder == null) continue;
 					System.IO.DirectoryInfo Info = System.IO.Directory.GetParent(Folder);

commit 8b6893d54f1a5a5fd502bb1c8cece7a2e8805e69

Fix: Potential infinite recursion in train detection.
If the train folder did not contain a train.dat but existed, https://github.com/leezer3/OpenBVE/commit/6b78dba6b48b80b115e0343db6b7af30a77fd90c caused infinite recursion.
Detect that we've looked for misplaced train folders and break off.

openBVE/OpenBve/UserInterface/formMain.Start.cs
@@ -893,6 +893,7 @@ namespace OpenBve {
 			} catch {
 				Folder = null;
 			}
+			bool recursionTest = false;
 			try {
 				while (true) {
 					string TrainFolder = OpenBveApi.Path.CombineDirectory(Folder, "Train");
@@ -915,12 +916,18 @@ namespace OpenBve {
 									ShowTrain(false);
 									return;
 								}
+								if (recursionTest)
+								{
+									break;
+								}
+								
 							}
 							else if (Folder.ToLowerInvariant().Contains("\\railway\\"))
 							{
 								//If we have a misplaced Train folder in either our Railway\Route
 								//or Railway folders, this can cause the train search to fail
 								//Detect the presence of a railway folder and carry on traversing upwards if this is the case
+								recursionTest = true;
 								Folder = OldFolder;
 							}
 							else