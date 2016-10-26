0fe96303d97ff8cb8736ca6a5f42f56d3713725b
Christopher Lees <leezer3@gmail.com>
2016-10-26 17:32:31 +0100

    Change: Fix potential edge case crash in texture loads.
    Change: A few comments in backgrounds

--- openBVE/OpenBve/Simulation/World/Background.cs
+++ openBVE/OpenBve/Simulation/World/Background.cs
@@ -20,8 +20,9 @@
 		internal static Background CurrentBackground = new Background(null, 6, false);
 		/// <summary>The new background texture (Currently fading in)</summary>
 		internal static Background TargetBackground = new Background(null, 6, false);
-
+		/// <summary>Defines the time in seconds taken for a new background to fade in</summary>
 		internal const double TargetBackgroundDefaultCountdown = 0.8;
+		/// <summary>The time remaining before the current background is at 100 % opacity</summary>
 		internal static double TargetBackgroundCountdown;
 	}
 }
