e15b98eb08cdb652da5e9eba332be828409627c0
Christopher Lees <leezer3@gmail.com>
2016-10-28 14:12:06 +0100

    Fix: Dynamic brightnesses which wrapped around were broken.

--- openBVE/OpenBve/Graphics/Renderer/Lighting.cs
+++ openBVE/OpenBve/Graphics/Renderer/Lighting.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using OpenBveApi.Colors;
 using OpenBveApi.Math;
 using OpenTK.Graphics.OpenGL;
@@ -73,26 +74,41 @@ namespace OpenBve
 			}
 			//Run through the array
 			int j = 0;
-			for (int i = 0; i < LightDefinitions.Length; i++)
+			for (int i = j; i < LightDefinitions.Length; i++)
 			{
 				
-				if (Time > LightDefinitions[i].Time)
+				if (Time < LightDefinitions[i].Time)
 				{
 					break;
 				}
 				j = i;
+
 			}
+
 			//We now know that our light definition is between the values defined in j and j + 1 (Or 0 if this is the end of the array)
 			int k;
-			if (j < LightDefinitions.Length)
+
+			if (j == 0)
 			{
-				k = j + 1;
+				//Our NEW light is to be the first entry in the array
+				//This means the OLD light is the last entry, but j and k do not need reversing
+				k = 0;
+				j = LightDefinitions.Length - 1;
+			}
+			else if (j == LightDefinitions.Length - 1)
+			{
+				//We are wrapping around to the end of the array
+				//Reverse j and k, as we have not yet passed the time for the first array entry
+				j = 0;
+				k = LightDefinitions.Length - 1;
 			}
 			else
 			{
-				k = 0;
+				//Somewhere in the middle, so the NEW light is simply one greater
+				k = j + 1;
 			}
 			int t1 = LightDefinitions[j].Time, t2 = LightDefinitions[k].Time;
+			
 			double cb1 = LightDefinitions[j].CabBrightness, cb2 = LightDefinitions[k].Time;
 			//Calculate, inverting if necessary
 
@@ -106,8 +122,17 @@ namespace OpenBve
 				t1 = 1;
 			}
 			//Calculate the percentage
-			var mu = (Time - t1)/(t2 - t1);
-			
+			double mu;
+			if (k == LightDefinitions.Length - 1)
+			{
+				//Wrapping around
+				mu = (86400 - Time + t1) / (86400 - t2 + t1);
+				
+			}
+			else
+			{
+				mu = (Time - t1) / (t2 - t1);
+			}
 			//Calculate the final colors and positions
 			OptionDiffuseColor = Color24.CosineInterpolate(LightDefinitions[j].DiffuseColor, LightDefinitions[k].DiffuseColor, mu);
 			OptionAmbientColor = Color24.CosineInterpolate(LightDefinitions[j].AmbientColor, LightDefinitions[k].AmbientColor, mu);
