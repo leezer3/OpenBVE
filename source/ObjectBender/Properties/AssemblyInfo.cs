d4b64fce6d06ecd2ceee9554d30897517b20934e
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2016-12-10 18:56:40 -0500

    Re-add assembly info as linux builds need them.

---
+++ source/ObjectBender/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ObjectBender")]
+[assembly: AssemblyProduct("ObjectBender")]
+[assembly: AssemblyCopyright("The openBVE Project")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
---
+++ source/OpenBveApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("openBVE API Class Library")]
+[assembly: AssemblyProduct("openBVE")]
+[assembly: AssemblyCopyright("The openBVE Project")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.4.4.0")]
