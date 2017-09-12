cf6adb9e997b85521f0558899ee5c5ac052d7547
Christopher Lees <leezer3@gmail.com>
2017-09-12 13:41:35 +0100

    Change: Separate events into distinct files

---
+++ source/OpenBVE/Game/Events/EventTypes/FogChange.cs
@@ -0,0 +1,41 @@
+﻿namespace OpenBve
+{
+	internal static partial class TrackManager
+	{
+		/// <summary>Is called when the in-game fog should be changed</summary>
+		internal class FogChangeEvent : GeneralEvent
+		{
+			/// <summary>The fog which applies previously to this point</summary>
+			internal Game.Fog PreviousFog;
+			/// <summary>The fog which applies after this point</summary>
+			internal Game.Fog CurrentFog;
+			/// <summary>The next upcoming fog (Used for distance based interpolation)</summary>
+			internal Game.Fog NextFog;
+
+			internal FogChangeEvent(double TrackPositionDelta, Game.Fog PreviousFog, Game.Fog CurrentFog, Game.Fog NextFog)
+			{
+				this.TrackPositionDelta = TrackPositionDelta;
+				this.DontTriggerAnymore = false;
+				this.PreviousFog = PreviousFog;
+				this.CurrentFog = CurrentFog;
+				this.NextFog = NextFog;
+			}
+			internal override void Trigger(int Direction, EventTriggerType TriggerType, TrainManager.Train Train, int CarIndex)
+			{
+				if (TriggerType == EventTriggerType.Camera)
+				{
+					if (Direction < 0)
+					{
+						Game.PreviousFog = this.PreviousFog;
+						Game.NextFog = this.CurrentFog;
+					}
+					else if (Direction > 0)
+					{
+						Game.PreviousFog = this.CurrentFog;
+						Game.NextFog = this.NextFog;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ source/OpenBVE/Game/Events/EventTypes/TrackEnd.cs
@@ -0,0 +1,26 @@
+﻿namespace OpenBve
+{
+	internal static partial class TrackManager
+	{
+		/// <summary>This event is placed at the end of the track</summary>
+		internal class TrackEndEvent : GeneralEvent
+		{
+			internal TrackEndEvent(double TrackPositionDelta)
+			{
+				this.TrackPositionDelta = TrackPositionDelta;
+				this.DontTriggerAnymore = false;
+			}
+			internal override void Trigger(int Direction, EventTriggerType TriggerType, TrainManager.Train Train, int CarIndex)
+			{
+				if (TriggerType == EventTriggerType.RearCarRearAxle & Train != TrainManager.PlayerTrain)
+				{
+					Train.Dispose();
+				}
+				else if (Train == TrainManager.PlayerTrain)
+				{
+					Train.Derail(CarIndex, 0.0);
+				}
+			}
+		}
+	}
+}
