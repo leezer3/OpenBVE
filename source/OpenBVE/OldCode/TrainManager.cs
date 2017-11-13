e19eb4cb7f41d2027991ef24495e33a3d7c8de9b
Christopher Lees <leezer3@gmail.com>
2017-11-13 12:58:46 +0000

    Change: Separate Axle & Coupler from the TrainManager lump
    [ci skip]

--- source/OpenBVE/OldCode/TrainManager.cs
+++ source/OpenBVE/OldCode/TrainManager.cs
@@ -8,20 +8,6 @@ namespace OpenBve
 	/// <summary>The TrainManager is the root class containing functions to load and manage trains within the simulation world.</summary>
 	public static partial class TrainManager
 	{
-		// axle
-		internal struct Axle
-		{
-			internal TrackManager.TrackFollower Follower;
-			internal bool CurrentWheelSlip;
-			internal double Position;
-		}
-
-		// coupler
-		internal struct Coupler
-		{
-			internal double MinimumDistanceBetweenCars;
-			internal double MaximumDistanceBetweenCars;
-		}
 
 		// cars
 		
---
+++ source/OpenBVE/Simulation/TrainManager/Car/Coupler.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenBve
+{
+	/// <summary>The TrainManager is the root class containing functions to load and manage trains within the simulation world.</summary>
+	public static partial class TrainManager
+	{
+		// coupler
+		internal struct Coupler
+		{
+			internal double MinimumDistanceBetweenCars;
+			internal double MaximumDistanceBetweenCars;
+		}
+	}
+}
