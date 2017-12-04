5c0311ce29385d5511ccc21a369f90e0098906df
Christopher Lees <leezer3@gmail.com>
2017-12-04 12:53:25 +0000

    Change: Cleanup in the csproj file

--- source/OpenBVE/Audio/Sounds.cs
+++ source/OpenBVE/Audio/Sounds.cs
@@ -8,19 +8,6 @@ namespace OpenBve
 {
 	internal static partial class Sounds
 	{
-
-		// --- enumerations ---
-
-		/// <summary>Represents different sound distance attenuation models.</summary>
-		internal enum SoundModels
-		{
-			/// <summary>Represents the new Inverse Distance Clamped Model.</summary>
-			Inverse = 0,
-			/// <summary>Represents the old Linear Distance Clamped Model.</summary>
-			Linear = -1
-		}
-
-
 		// --- members ---
 
 		/// <summary>The current OpenAL device.</summary>
