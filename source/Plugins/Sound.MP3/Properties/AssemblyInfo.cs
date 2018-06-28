c30cc6aa644305d0827de11a93c9be7d7f753d6f
Christopher Lees <leezer3@gmail.com>
2018-06-28 20:57:06 +0100

    New: Handle MP3 format sounds & Wave encapsulated MP3

--- source/Plugins/Sound.MP3/Properties/AssemblyInfo.cs
+++ source/Plugins/Sound.MP3/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("RIFF/WAV Sound Loader")]
-[assembly: AssemblyDescription("Supports loading WAV PCM or Microsoft ADPCM sounds embedded in a RIFF/RIFX container.")]
+[assembly: AssemblyTitle("MP3 Sound Loader")]
+[assembly: AssemblyDescription("Supports loading MP3 sounds.")]
 [assembly: AssemblyProduct("openBVE")]
 [assembly: AssemblyCopyright("The openBVE Project")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.0.0.0")]
