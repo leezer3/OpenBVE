48d7ac0bbfebf352f48f3ad02c3ecab7cfab6322
S520 <29241703+s520@users.noreply.github.com>
2018-09-01 16:06:38 +0900

    Fix: Build error at OpenBveAts/Train.cs on VS2013

--- source/Plugins/OpenBveAts/Train.cs
+++ source/Plugins/OpenBveAts/Train.cs
@@ -15,15 +15,15 @@ namespace Plugin {
 		internal class ReadOnlyHandles {
 			// --- properties ---
 			/// <summary>Gets or sets the reverser position.</summary>
-			internal int Reverser { get; }
+			internal int Reverser { get; private set; }
 			/// <summary>Gets or sets the power notch.</summary>
-			internal int PowerNotch { get; }
+			internal int PowerNotch { get; private set; }
 			/// <summary>Gets or sets the brake notch.</summary>
-			internal int BrakeNotch { get; }
+			internal int BrakeNotch { get; private set; }
 			/// <summary>Gets or sets the brake notch.</summary>
-			internal int LocoBrakeNotch { get; }
+			internal int LocoBrakeNotch { get; private set; }
 			/// <summary>Gets or sets whether the const speed system is enabled.</summary>
-			internal bool ConstSpeed { get; }
+			internal bool ConstSpeed { get; private set; }
 			// --- constructors ---
 			/// <summary>Creates a new instance of this class.</summary>
 			/// <param name="handles">The handles</param>
