2c5a3d90525310e0fe4e4f2449be269e94724341
Christopher Lees <leezer3@gmail.com>
2019-05-30 10:38:05 +0100

    Change: Update LibRender AssemblyInfo

--- source/LibRender/Properties/AssemblyInfo.cs
+++ source/LibRender/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LibRender")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("openBVE shared renderer library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibRender")]
+[assembly: AssemblyProduct("openBVE")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
