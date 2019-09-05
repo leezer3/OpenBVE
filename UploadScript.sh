26d2aa9f10bf7a8cdceb0e1e808c87051d9c444a
s520 <sony.pcv.s520@gmail.com>
2019-09-06 03:51:11 +0900

    Fix: Linux builds not uploading after Mono bump again...

--- UploadScript.sh
+++ UploadScript.sh
@@ -12,7 +12,7 @@ function wrap() {
   if [ "$TRAVIS_OS_NAME" = "linux" ] ;
     then
 	echo "Linux worker"
-	if [ "$mono_version" != "5.20.1" ] ;
+	if [ "$mono_version" != "5.20.1.19" ] ;
 		then
 		echo "Wrong Mono version- Not uploading this build"
 		exit
