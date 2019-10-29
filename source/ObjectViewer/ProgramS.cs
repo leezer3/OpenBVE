6e3ff9422dc2b78bfb7d62adeb7a72631c5dfd17
Christopher Lees <leezer3@gmail.com>
2019-10-29 11:06:08 +0000

    Fix: Object Viewer not retaining the background color when loading etc.

--- source/ObjectViewer/ProgramS.cs
+++ source/ObjectViewer/ProgramS.cs
@@ -263,6 +263,7 @@ namespace OpenBve {
 			Renderer.InitializeVisibility();
 			Renderer.UpdateVisibility(0.0, true);
 			ObjectManager.UpdateAnimatedWorldObjects(0.01, true);
+			Renderer.ApplyBackgroundColor();
 		}
 
 		internal static MouseState currentMouseState;
@@ -402,6 +403,7 @@ namespace OpenBve {
 	                Renderer.InitializeVisibility();
 	                Renderer.UpdateVisibility(0.0, true);
 	                ObjectManager.UpdateAnimatedWorldObjects(0.01, true);
+					Renderer.ApplyBackgroundColor();
 	                break;
 	            case Key.F7:
 	            {
@@ -512,6 +514,7 @@ namespace OpenBve {
 		            }
 					Dialog.Dispose();
 	            }
+					Renderer.ApplyBackgroundColor();
 	                break;
 	            case Key.F9:
 	                if (Interface.MessageCount != 0)
@@ -527,6 +530,7 @@ namespace OpenBve {
 	                Renderer.TextureManager.UnloadAllTextures();
 	                Interface.ClearMessages();
 	                Files = new string[] {};
+					Renderer.ApplyBackgroundColor();
 	                break;
 	            case Key.Left:
 	                RotateX = -1;
