4d8826162928f71a278255f5b7855adb350e8374
Christopher Lees <leezer3@gmail.com>
2019-11-28 13:08:59 +0000

    Add basic fuel tank struct

---
+++ source/OpenBVE/Simulation/TrainManager/Motor/FuelTank.cs
@@ -0,0 +1,24 @@
+﻿namespace OpenBve
+{
+	/// <summary>The TrainManager is the root class containing functions to load and manage trains within the simulation world.</summary>
+	public static partial class TrainManager
+	{
+		/// <summary>Represents a fuel tank</summary>
+		public class FuelTank
+		{
+			/// <summary>The minimum fuel level</summary>
+			public readonly double MinimumLevel;
+			/// <summary>The maximum fuel level</summary>
+			public readonly double MaximumLevel;
+			/// <summary>The current fuel level</summary>
+			public double CurrentLevel;
+
+			public FuelTank(double min, double max, double current)
+			{
+				MinimumLevel = min;
+				MaximumLevel = max;
+				CurrentLevel = current;
+			}
+		}
+	}
+}
