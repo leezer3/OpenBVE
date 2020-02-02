4e2fe741a0bf12252981fa1fa503c9bb3e38906a
Christopher Lees <leezer3@gmail.com>
2020-02-02 21:25:09 +0000

    Fix: Object disposal fix incomplete
    https://github.com/leezer3/OpenBVE/commit/7802c1156514a83b889f86e002c4f2658fbf3d0e

--- source/OpenBveApi/Objects/ObjectTypes/AnimatedObjectCollection.cs
+++ source/OpenBveApi/Objects/ObjectTypes/AnimatedObjectCollection.cs
@@ -64,7 +64,7 @@ namespace OpenBveApi.Objects
 								Matrix4D mat = Matrix4D.Identity;
 								mat *= Objects[i].States[0].Translation;
 								mat *= (Matrix4D)new Transformation(FinalTransformation.Z, FinalTransformation.Y, FinalTransformation.X);
-								double zOffset = mat.ExtractTranslation().Z * -1.0;
+								double zOffset = mat.ExtractTranslation().Z;
 								
 								currentHost.CreateStaticObject(Objects[i].States[0].Prototype, Position, AuxTransformation, mat, Matrix4D.CreateTranslation(Position.X, Position.Y, -Position.Z), AccurateObjectDisposal, zOffset, StartingDistance, EndingDistance, BlockLength, TrackPosition, Brightness);
 							}
