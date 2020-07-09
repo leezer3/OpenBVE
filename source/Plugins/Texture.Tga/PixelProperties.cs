64b16d94d685a95b0c89cffd2648e7208b106410
Christopher Lees <leezer3@gmail.com>
2020-07-09 11:29:22 +0100

    Fix: Some TGA files broken
    Closes https://github.com/leezer3/OpenBVE/issues/499

---
+++ source/Plugins/Texture.Tga/PixelProperties.cs
@@ -0,0 +1,41 @@
+﻿namespace Plugin
+{
+	internal enum VerticalTransferOrder
+	{
+		/// <summary>Unknown</summary>
+		Unknown = -1,
+		/// <summary>Bottom to top</summary>
+		Bottom = 0,
+		/// <summary>Top to bottom</summary>
+		Top = 1
+	}
+
+	internal enum HorizontalTransferOrder
+	{
+		/// <summary>Unknown</summary>
+		Unknown = -1,
+		/// <summary>Right to left</summary>
+		Right = 0,
+		/// <summary>Left to right</summary>
+		Left = 1
+	}
+
+	internal enum FirstPixelDestination
+	{
+		/// <summary>Unknown- Assume bottom right</summary>
+		/// <remarks>Right to left, bottom to top pixel order</remarks>
+		Unknown = -1,
+		/// <summary>Bottom left</summary>
+		/// <remarks>Left to right, bottom to top pixel order</remarks>
+		BottomLeft = 0,
+		/// <summary>Bottom right</summary>
+		/// /// <remarks>Right to left, bottom to top pixel order</remarks>
+		BottomRight = 1,
+		/// <summary>Top left</summary>
+		/// <remarks>Left to right, top to bottom pixel order</remarks>
+		TopLeft = 2,
+		/// <summary>Top right</summary>
+		/// <remarks>Right to left, top to bottom pixel order</remarks>
+		TopRight = 3
+	}
+}
