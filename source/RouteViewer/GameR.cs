6fcfb8a658bc8bc0a94d95ca691d5e9b1f8803ce
S520 <29241703+s520@users.noreply.github.com>
2020-08-22 21:46:30 +0900

    Fix: issue where objects aren't reloaded in RouteViewer

--- source/RouteViewer/GameR.cs
+++ source/RouteViewer/GameR.cs
@@ -31,7 +31,7 @@ namespace OpenBve {
 		// ================================
 
 		internal static void Reset() {
-			Program.Renderer.Initialize(Program.CurrentHost, Interface.CurrentOptions);
+			Program.Renderer.Reset();
 			// track manager
 			Program.CurrentRoute.Tracks = new Dictionary<int, Track>();
 			Track t = new Track
