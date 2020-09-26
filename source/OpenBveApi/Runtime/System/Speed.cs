1e9bb39c2628293700122109a577d33dd7b47238
Christopher Lees <leezer3@gmail.com>
2020-09-26 11:25:32 +0100

    Change: Split runtime into separate files

---
+++ source/OpenBveApi/Runtime/System/Speed.cs
@@ -0,0 +1,43 @@
+﻿namespace OpenBveApi.Runtime
+{
+	/// <summary>Represents a speed.</summary>
+	public class Speed
+	{
+		/// <summary>The speed in meters per second.</summary>
+		private readonly double MyValue;
+
+		/// <summary>Gets the speed in meters per second.</summary>
+		public double MetersPerSecond
+		{
+			get
+			{
+				return this.MyValue;
+			}
+		}
+
+		/// <summary>Gets the speed in kilometes per hour.</summary>
+		public double KilometersPerHour
+		{
+			get
+			{
+				return 3.6 * this.MyValue;
+			}
+		}
+
+		/// <summary>Gets the speed in miles per hour.</summary>
+		public double MilesPerHour
+		{
+			get
+			{
+				return 2.236936 * this.MyValue;
+			}
+		}
+
+		/// <summary>Creates a new instance of this class.</summary>
+		/// <param name="value">The speed in meters per second.</param>
+		public Speed(double value)
+		{
+			this.MyValue = value;
+		}
+	}
+}
---
+++ source/OpenBveApi/Runtime/Train/BrakeTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenBveApi.Runtime
+{
+	/// <summary>Represents the type of brake the train uses.</summary>
+	public enum BrakeTypes
+	{
+		/// <summary>The train uses the electromagnetic straight air brake. The numerical value of this constant is 0.</summary>
+		ElectromagneticStraightAirBrake = 0,
+		/// <summary>The train uses the analog/digital electro-pneumatic air brake without a brake pipe (electric command brake). The numerical value of this constant is 1.</summary>
+		ElectricCommandBrake = 1,
+		/// <summary>The train uses the automatic air brake with partial release. The numerical value of this constant is 2.</summary>
+		AutomaticAirBrake = 2
+	}
+}
