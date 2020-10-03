362d9a07b923a275a350f492aeab6c5b4ff2bd2c
Christopher Lees <leezer3@gmail.com>
2020-10-03 12:18:01 +0100

    New: Implement windscreen wipers

---
+++ source/OpenBVE/Simulation/TrainManager/Train/Windscreen.Wiper.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using SoundManager;
+
+namespace OpenBve
+{
+	internal class WindscreenWiper
+	{
+		/// <summary>The car sound played when 20% or less drops are visible</summary>
+		internal CarSound DryWipeSound;
+		/// <summary>The car sound played when 20% or more drops are visible</summary>
+		internal CarSound WetWipeSound;
+		/// <summary>The car sound played when the wipers are activated or deactivated</summary>
+		internal CarSound WiperSwitchSound;
+		/// <summary>The speed taken for the wiper to pass across the screen</summary>
+		internal double WipeSpeed = 1000;
+		/// <summary>The time for which the wiper pauses at the hold position</summary>
+		internal double WiperHoldTime = 0;
+		/// <summary>The wiper rest position</summary>
+		internal WiperPosition RestPosition;
+		/// <summary>The wiper hold position</summary>
+		internal WiperPosition HoldPosition;
+		/// <summary>The current wiper position</summary>
+		/// <remarks>Range of 0 to 100</remarks>
+		internal int CurrentPosition;
+		/// <summary>The time taken to move 1 position unit in milliseconds</summary>
+		internal double MovementSpeed = 10;
+		/// <summary>The current speed</summary>
+		internal WiperSpeed CurrentSpeed;
+
+		private readonly Windscreen Windscreen;
+		private WiperPosition currentDirection;
+		private double wiperTimer;
+		private double holdTimer;
+		private bool soundTriggered;
+
+		internal WindscreenWiper(Windscreen windscreen, WiperPosition restPosition, WiperPosition holdPosition)
+		{
+			RestPosition = restPosition;
+			HoldPosition = holdPosition;
+			Windscreen = windscreen;
+		}
+
+		internal void Update(double TimeElapsed)
+		{
+			wiperTimer += TimeElapsed;
+			//Move the wiper
+			if (wiperTimer > MovementSpeed)
+			{
+				wiperTimer = 0;
+				switch (currentDirection)
+				{
+					case WiperPosition.Left:
+						if (CurrentPosition > 0)
+						{
+							CurrentPosition--;
+						}
+						break;
+					case WiperPosition.Right:
+						if (CurrentPosition < 100)
+						{
+							CurrentPosition++;
+						}
+						break;
+				}
+				soundTriggered = false;
+			}
+			switch (CurrentPosition)
+			{
+				//When the wiper is at either end of the travel, determine what to do next
+				case 0:
+					if (CurrentSpeed > 0)
+					{
+						if (HoldPosition == WiperPosition.Left && CurrentSpeed != WiperSpeed.Fast)
+						{
+							holdTimer += TimeElapsed;
+							if (holdTimer > WiperHoldTime)
+							{
+								holdTimer = 0;
+								currentDirection = WiperPosition.Right;
+							}
+						}
+						else
+						{
+							currentDirection = WiperPosition.Right;
+						}
+					}
+					else
+					{
+						if (RestPosition == WiperPosition.Right)
+						{
+							if (HoldPosition == WiperPosition.Left)
+							{
+								holdTimer += TimeElapsed;
+								if (holdTimer > WiperHoldTime)
+								{
+									holdTimer = 0;
+									currentDirection = WiperPosition.Right;
+								}
+							}
+							else
+							{
+								currentDirection = WiperPosition.Right;
+							}
+						}
+					}
+					break;
+				case 100:
+					if (CurrentSpeed > 0)
+					{
+						if (HoldPosition == WiperPosition.Right && CurrentSpeed != WiperSpeed.Fast)
+						{
+							holdTimer += TimeElapsed;
+							if (holdTimer > WiperHoldTime)
+							{
+								holdTimer = 0;
+								currentDirection = WiperPosition.Left;
+							}
+						}
+						else
+						{
+							currentDirection = WiperPosition.Left;
+						}
+					}
+					else
+					{
+						if (RestPosition == WiperPosition.Left)
+						{
+							if (HoldPosition == WiperPosition.Right)
+							{
+								holdTimer += TimeElapsed;
+								if (holdTimer > WiperHoldTime)
+								{
+									holdTimer = 0;
+									currentDirection = WiperPosition.Left;
+								}
+							}
+							else
+							{
+								currentDirection = WiperPosition.Left;
+							}
+						}
+					}
+					break;
+				//Wiper has started moving, determine which sound effect to play
+				case 1:
+					if (currentDirection == WiperPosition.Right)
+					{
+						if(Windscreen.currentDrops / (double)Windscreen.RainDrops.Length > 0.8)
+						{
+							if (WetWipeSound.Buffer != null && soundTriggered == false)
+							{
+								Program.Sounds.PlaySound(WetWipeSound.Buffer, 1.0, 1.0, WetWipeSound.Position, Windscreen.Car, false);
+								soundTriggered = true;
+							}
+						}
+						else
+						{
+							if (DryWipeSound.Buffer != null && soundTriggered == false)
+							{
+								Program.Sounds.PlaySound(WetWipeSound.Buffer, 1.0, 1.0, DryWipeSound.Position, Windscreen.Car, false);
+								soundTriggered = true;
+							}
+						}
+					}
+					break;
+				case 99:
+					if (currentDirection == WiperPosition.Left)
+					{
+						if(Windscreen.currentDrops / (double)Windscreen.RainDrops.Length > 0.8)
+						{
+							if (WetWipeSound.Buffer != null && soundTriggered == false)
+							{
+								Program.Sounds.PlaySound(WetWipeSound.Buffer, 1.0, 1.0, WetWipeSound.Position, Windscreen.Car, false);
+								soundTriggered = true;
+							}
+						}
+						else
+						{
+							if (DryWipeSound.Buffer != null && soundTriggered == false)
+							{
+								Program.Sounds.PlaySound(WetWipeSound.Buffer, 1.0, 1.0, DryWipeSound.Position, Windscreen.Car, false);
+								soundTriggered = true;
+							}
+						}
+					}
+					break;
+			}
+			int dropToRemove = Math.Min(Windscreen.RainDrops.Length - 1, (int) (CurrentPosition / (100.0 / Windscreen.RainDrops.Length)));
+			if (Windscreen.RainDrops[dropToRemove])
+			{
+				Windscreen.RainDrops[dropToRemove] = false;
+				Windscreen.currentDrops--;
+			}
+		}
+	}
+}
---
+++ source/OpenBVE/Simulation/TrainManager/Train/Windscreen.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using SoundManager;
+
+namespace OpenBve
+{
+	/// <summary>Represents a train windscreen</summary>
+	internal class Windscreen
+	{
+		/// <summary>Whether currently raining</summary>
+		internal bool CurrentlyRaining;
+		/// <summary>The current rain intensity</summary>
+		internal int RainIntensity;
+		/// <summary>The raindrop array</summary>
+		internal bool[] RainDrops;
+		/// <summary>The sound played when a raindrop hits the windscreen</summary>
+		internal CarSound DropSound;
+		/// <summary>The windscreen wipers</summary>
+		internal WindscreenWiper Wipers;
+
+		internal readonly TrainManager.Car Car;
+		
+		private double dropTimer;
+		internal int currentDrops;
+
+		internal Windscreen(int numberOfDrops, TrainManager.Car car)
+		{
+			RainDrops = new bool[numberOfDrops];
+			currentDrops = 0;
+			Car = car;
+
+		}
+
+		internal void Update(double TimeElapsed)
+		{
+			if (RainDrops == null || RainDrops.Length == 0)
+			{
+				return;
+			}
+
+			if (CurrentlyRaining)
+			{
+				int nextDrop = PickDrop();
+				dropTimer += TimeElapsed;
+				var dev = (int)(0.4 * 2000 / RainIntensity);
+				int dropInterval = 2000 / RainIntensity + Program.RandomNumberGenerator.Next(dev, dev * 2);
+				if (dropTimer > dropInterval)
+				{
+					if (nextDrop != -1)
+					{
+						RainDrops[nextDrop] = true;
+						Program.Sounds.PlayCarSound(DropSound, 1.0, 1.0, Car, false);
+						currentDrops++;
+					}
+					dropTimer = 0.0;
+				}
+			}
+			Wipers.Update(TimeElapsed);
+			
+		}
+
+		private int PickDrop()
+		{
+			List<int> availableDrops = new List<int>();
+			for (int i = 0; i < RainDrops.Length; i++)
+			{
+				if (RainDrops[i] == false)
+				{
+					availableDrops.Add(i);
+				}
+			}
+			return availableDrops.Count != 0 ? availableDrops[Program.RandomNumberGenerator.Next(availableDrops.Count)] : -1;
+		}
+	}
+}
