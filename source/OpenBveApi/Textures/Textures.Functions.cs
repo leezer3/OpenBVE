6c83e03a367ba8d1552b4e54cc7a40fe8ce1c86f
Christopher Lees <leezer3@gmail.com>
2021-11-23 11:53:48 +0000

    Fix: Multiple frame textures not working in some cases

--- source/OpenBveApi/Textures/Textures.Functions.cs
+++ source/OpenBveApi/Textures/Textures.Functions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.ServiceModel.Security;
 using OpenBveApi.Colors;
 
 namespace OpenBveApi.Textures {
@@ -130,41 +129,56 @@ namespace OpenBveApi.Textures {
 						break;
 				}
 			}
-		    if (texture.BitsPerPixel == 32) {
-		        int width = texture.Width;
-		        int height = texture.Height;
-		        byte[] source = texture.Bytes;
-		        byte[] target = new byte[4 * width * height];
-		        byte r = color.Value.R;
-		        byte g = color.Value.G;
-		        byte b = color.Value.B;
-		        if (source[0] == r && source[1] == g && source[2] == b) {
-		            target[0] = 128;
-		            target[1] = 128;
-		            target[2] = 128;
-		            target[3] = 0;
-		        } else {
-		            target[0] = source[0];
-		            target[1] = source[1];
-		            target[2] = source[2];
-		            target[3] = source[3];
-		        }
-		        for (int i = 4; i < source.Length; i += 4) {
-		            if (source[i] == r && source[i + 1] == g && source[i + 2] == b) {
-		                target[i + 0] = target[i - 4];
-		                target[i + 1] = target[i - 3];
-		                target[i + 2] = target[i - 2];
-		                target[i + 3] = 0;
-		            } else {
-		                target[i + 0] = source[i + 0];
-		                target[i + 1] = source[i + 1];
-		                target[i + 2] = source[i + 2];
-		                target[i + 3] = source[i + 3];
-		            }
-		        }
-		        return new Texture(width, height, 32, target, texture.Palette);
-		    }
+			if (texture.BitsPerPixel == 32)
+			{
+				if (texture.MultipleFrames)
+				{
+					byte[][] newFrames = new byte[texture.TotalFrames][];
+					for (int i = 0; i < texture.TotalFrames; i++)
+					{
+						newFrames[i] = ApplyTransparentColor(texture.Bytes, texture.Width, texture.Height, color.Value);
+						texture.CurrentFrame++;
+					}
+
+					return new Texture(texture.Width, texture.Height, 32, newFrames, texture.FrameInterval);
+				}
+				return new Texture(texture.Width, texture.Height, 32, ApplyTransparentColor(texture.Bytes, texture.Width, texture.Height, color.Value), texture.Palette);
+
+			}
 		    throw new NotSupportedException();
+		}
+
+		private static byte[] ApplyTransparentColor(byte[] source, int width, int height, Color24 color)
+		{
+			byte[] target = new byte[4 * width * height];
+			byte r = color.R;
+			byte g = color.G;
+			byte b = color.B;
+			if (source[0] == r && source[1] == g && source[2] == b) {
+				target[0] = 128;
+				target[1] = 128;
+				target[2] = 128;
+				target[3] = 0;
+			} else {
+				target[0] = source[0];
+				target[1] = source[1];
+				target[2] = source[2];
+				target[3] = source[3];
+			}
+			for (int i = 4; i < source.Length; i += 4) {
+				if (source[i] == r && source[i + 1] == g && source[i + 2] == b) {
+					target[i + 0] = target[i - 4];
+					target[i + 1] = target[i - 3];
+					target[i + 2] = target[i - 2];
+					target[i + 3] = 0;
+				} else {
+					target[i + 0] = source[i + 0];
+					target[i + 1] = source[i + 1];
+					target[i + 2] = source[i + 2];
+					target[i + 3] = source[i + 3];
+				}
+			}
+			return target;
 		}
 	}
 }
