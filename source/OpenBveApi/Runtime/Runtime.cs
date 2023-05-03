0275fbaecd12d6f91ae201eed1a2551bdde3aeb8
Christopher Lees <leezer3@users.noreply.github.com>
2023-05-03 22:24:57 +0100

    New: Add overload to play multi-car sounds (#862)

--- source/OpenBveApi/Runtime/Runtime.cs
+++ source/OpenBveApi/Runtime/Runtime.cs
@@ -25,6 +25,16 @@ namespace OpenBveApi.Runtime {
     /// <returns>The handle to the sound, or a null reference if the sound could not be played.</returns>
     /// <exception cref="System.InvalidOperationException">Raised when the host application does not allow the function to be called.</exception>
     public delegate SoundHandle PlayCarSoundDelegate(int index, double volume, double pitch, bool looped, int carIndex);
+
+    /// <summary>Plays a sound.</summary>
+    /// <param name="index">The index to the sound to be played.</param>
+    /// <param name="volume">The initial volume of the sound. A value of 1.0 represents nominal volume.</param>
+    /// <param name="pitch">The initial pitch of the sound. A value of 1.0 represents nominal pitch.</param>
+    /// <param name="looped">Whether the sound should be played in an indefinate loop.</param>
+    /// <param name="carIndicies">An array of car indicies to start playback on</param>
+    /// <returns>The handle to the sound, or a null reference if the sound could not be played.</returns>
+    /// <exception cref="System.InvalidOperationException">Raised when the host application does not allow the function to be called.</exception>
+    public delegate SoundHandle[] PlayMultipleCarSoundDelegate(int index, double volume, double pitch, bool looped, int[] carIndicies);
 	
 	/// <summary>Adds a message to the in-game display</summary>
 	/// <param name="Message">The message to display</param>
