aedbafc7fac87bfbe50f20ebfb31e656c22e7dc0
Christopher Lees <leezer3@gmail.com>
2023-06-20 19:35:12 +0100

    Add AmbientTemperature variable

--- source/OpenBveApi/FunctionScripts/FunctionScript.cs
+++ source/OpenBveApi/FunctionScripts/FunctionScript.cs
@@ -851,6 +851,11 @@ namespace OpenBveApi.FunctionScripting
 							if (n >= InstructionSet.Length) Array.Resize(ref InstructionSet, InstructionSet.Length << 1);
 							InstructionSet[n] = Instructions.SandShots;
 							n++; s++; if (s >= m) m = s; break;
+						case "ambienttemperature":
+							if (n >= InstructionSet.Length) Array.Resize(ref InstructionSet, InstructionSet.Length << 1);
+							InstructionSet[n] = Instructions.AmbientTemperature;
+							n++; s++; if (s >= m) m = s; break;
+							break;
 						// default
 						default:
 							throw new System.IO.InvalidDataException("Unknown command " + Arguments[i] + " encountered in function script " + Expression);
