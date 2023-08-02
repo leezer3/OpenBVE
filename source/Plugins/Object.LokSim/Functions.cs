9aef7cc1f3d69bdf26767e3aa741f15af52790a3
Christopher Lees <leezer3@gmail.com>
2023-08-02 18:55:11 +0100

    Handle triple quotes at end of LokSim tag

--- source/Plugins/Object.LokSim/Functions.cs
+++ source/Plugins/Object.LokSim/Functions.cs
@@ -12,17 +12,23 @@ namespace Plugin
 			
 			StringBuilder builder = new StringBuilder();
 			builder.Append(s[0]);
-			for (int i = 1; i < s.Length - 1; i++)
+			for (int i = 1; i < s.Length - 2; i++)
 			{
 				char next = s[i];
 				char second = s[i + 1];
+				char third = s[i + 2];
 				char last = s[i - 1];
 				if (next == '"' && second == '"' && last != '=')
 				{
 					/*
 					 * LokSim allows the use of paired quotes within the FileInfo tag
-					 * This is not valid XML, but some stuff (e.g. AndreasZ taurus uses it
+					 * This is not valid XML, but some stuff (e.g. AndreasZ taurus) uses it
 					 */
+					if (third == '"')
+					{
+						// *stupid* variant- triple quotes at end of tag
+						builder.Append('"');
+					}
 					i+= 2;
 				}
 				else
@@ -31,7 +37,9 @@ namespace Plugin
 				}
 					
 			}
+			builder.Append(s[s.Length - 2]);
 			builder.Append(s[s.Length - 1]);
+			string sv = builder.ToString();
 			return builder.ToString();
 
 		}
