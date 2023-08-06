dc183a7f31eed30346d1b4a86363f0e375b341d4
Christopher Lees <leezer3@gmail.com>
2023-08-06 19:17:41 +0100

    WIP: Move Menu to LibRender2
    Android requires a separate binary, so this can't live in the main logic tree

---
+++ source/LibRender2/Menu/MenuCaption.cs
@@ -0,0 +1,11 @@
+﻿namespace LibRender2.Menu
+{
+	/// <summary>A caption to be rendered at the top of the menu</summary>
+	public class MenuCaption : MenuEntry
+	{
+		public MenuCaption(string Text)
+		{
+			this.Text = Text;
+		}
+	}
+}
---
+++ source/LibRender2/Menu/MenuOption.Type.cs
@@ -0,0 +1,19 @@
+﻿namespace LibRender2.Menu
+{
+	/// <summary>The type of menu option</summary>
+	public enum OptionType
+	{
+		/// <summary>Sets the screen resolution</summary>
+		ScreenResolution,
+		/// <summary>Sets the viewing distance</summary>
+		ViewingDistance,
+		/// <summary>Toggles full-screen mode</summary>
+		FullScreen,
+		/// <summary>Sets the interpolation level</summary>
+		Interpolation,
+		/// <summary>Sets the anisotropic filtering level</summary>
+		AnisotropicLevel,
+		/// <summary>Sets the antialiasing level</summary>
+		AntialiasingLevel
+	}
+}
