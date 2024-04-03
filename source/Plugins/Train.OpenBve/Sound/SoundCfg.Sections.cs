d144bdec8179aa418df88566f202460d616ba041
Christopher Lees <leezer3@gmail.com>
2024-04-03 17:15:45 +0100

    Change: Convert Sound.cfg to enum loading, detect invalid section

---
+++ source/Plugins/Train.OpenBve/Sound/SoundCfg.Sections.cs
@@ -0,0 +1,44 @@
+﻿namespace Train.OpenBve
+{
+	internal enum SoundCfgSection
+	{
+		/// <summary>Unknown section</summary>
+		Unknown = 0,
+		/// <summary>Run sounds</summary>
+		Run,
+		/// <summary>Flange sounds</summary>
+		Flange,
+		/// <summary>Motor sounds</summary>
+		Motor,
+		/// <summary>Switch sounds</summary>
+		Switch,
+		/// <summary>Brake sounds</summary>
+		Brake,
+		/// <summary>Compressor sounds</summary>
+		Compressor,
+		/// <summary>Air suspension sounds</summary>
+		Suspension,
+		/// <summary>Horn sounds</summary>
+		Horn,
+		/// <summary>Door open / close sounds</summary>
+		Door,
+		/// <summary>ATS Plugin sounds</summary>
+		ATS,
+		/// <summary>Train ready to start buzzer sound</summary>
+		Buzzer,
+		/// <summary>Pilot lamp switch sound</summary>
+		PilotLamp,
+		/// <summary>Sounds played when the brake handle is moved</summary>
+		BrakeHandle,
+		/// <summary>Sounds played when the power handle is moved</summary>
+		MasterController,
+		/// <summary>Sounds played when the reverseer is moved</summary>
+		Reverser,
+		/// <summary>Breaker sounds</summary>
+		Breaker,
+		/// <summary>Miscellaneous sounds</summary>
+		Others,
+		/// <summary>Windscreen / raindrop sounds</summary>
+		Windscreen
+	}
+}
