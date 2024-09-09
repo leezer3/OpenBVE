6b3cb9ffc65a8af41c55d2113e4abf9fe377c5eb
Christopher Lees <leezer3@gmail.com>
2024-09-09 20:26:09 +0100

    Further cleaning

--- source/AssimpParser/Wavefront/ObjFileData.cs
+++ source/AssimpParser/Wavefront/ObjFileData.cs
@@ -128,9 +128,9 @@ namespace AssimpNET.Obj
 		//! Object name
 		public readonly string ObjName;
 		//! Transformation matrix, stored in OpenGL format
-#pragma warning disable 169
+#pragma warning disable 169, IDE0051
 		Matrix4D Transformation;
-#pragma warning restore 169
+#pragma warning restore 169, IDE0051
 		//! All sub-objects referenced by this object
 		readonly List<WavefrontObject> SubObjects = new List<WavefrontObject>();
 		/// Assigned meshes
@@ -223,7 +223,7 @@ namespace AssimpNET.Obj
 
 		public const uint NoMaterial = ~0u;
 		/// The name for the mesh
-		string Name;
+		readonly string Name;
 		/// Array with pointer to all stored faces
 		public List<Face> Faces = new List<Face>();
 		/// Assigned material
