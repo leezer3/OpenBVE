04a47641da12ecc7d400ffcc5ff82b1b6e5edb17
Christopher Lees <leezer3@gmail.com>
2024-11-08 18:33:41 +0000

    New: Add power supply framework to API

---
+++ source/OpenBveApi/Routes/PowerSupply.VoltageTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenBveApi.Routes
+{
+	/// <summary>The voltage types for any given conductor</summary>
+	public enum PowerSupplyVoltageTypes
+	{
+		/// <summary>Alternating current</summary>
+		AC,
+		/// <summary>Direct current</summary>
+		DC
+	}
+}
---
+++ source/OpenBveApi/Routes/PowerSupply.cs
@@ -0,0 +1,24 @@
+﻿namespace OpenBveApi.Routes
+{
+	/// <summary>The available power supplies for a track element</summary>
+	public class PowerSupply
+	{
+		/// <summary>The voltage type supplied</summary>
+		internal readonly PowerSupplyVoltageTypes VoltageType;
+		/// <summary>The voltage supplied</summary>
+		internal readonly double Voltage;
+		/// <summary>The maximum amperage supplied</summary>
+		internal readonly double Amperage;
+		/// <summary>The contact height</summary>
+		internal readonly double ContactHeight;
+
+		/// <summary>Creates a new power supply</summary>
+		public PowerSupply(PowerSupplyVoltageTypes voltageType, double voltage, double amperage, double contactHeight)
+		{
+			VoltageType = voltageType;
+			Voltage = voltage;
+			Amperage = amperage;
+			ContactHeight = contactHeight;
+		}
+	}
+}
