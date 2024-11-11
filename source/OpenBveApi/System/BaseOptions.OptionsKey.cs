d7641e79df1ac8abe733c301e04cef49b0e12489
Christopher Lees <leezer3@gmail.com>
2024-11-11 11:03:52 +0000

    Create OptionsKey enum

---
+++ source/OpenBveApi/System/BaseOptions.OptionsKey.cs
@@ -0,0 +1,92 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+namespace OpenBveApi
+{
+	/// <summary>The keys found in an options file</summary>
+    public enum OptionsKey
+    {
+		// Language
+		Code,
+		// Interface
+		Folder,
+		TimetableMode,
+		KioskMode,
+		KioskModeTimer,
+		Accessibility,
+		Font,
+		DailyBuildUpdates,
+		// Display
+		PreferNativeBackend,
+		Mode,
+		VSync,
+		WindowWidth,
+		WindowHeight,
+		FullScreenWidth,
+		FullScreenHeight,
+		FullScreenBits,
+		MainMenuWidth,
+		MainMenuHeight,
+		LoadInAdvance,
+		UnloadTextures,
+		ForwardsCompatibleContext,
+		IsUseNewRenderer,
+		ViewingDistance,
+		QuadLeafSize,
+		UIScaleFactor,
+		// Quality
+		Interpolation,
+		AnisotropicFilteringLevel,
+		AntialisingLevel,
+		TransparencyMode,
+		OldTransparencyMode,
+		MotionBlur,
+		FPSLimit,
+		// Object Optimization
+		BasicThreshold,
+		FullThreshold,
+		VertexCulling,
+		// Simulation
+		Toppling,
+		Collisions,
+		Derailments,
+		LoadingSway,
+		BlackBox,
+		AcceleratedTimeFactor,
+		EnableBVETSHacks,
+		EnableBVE5ScriptedTrain,
+		// Controls
+		UseJoysticks,
+		JoystickAxisEB,
+		JoystickAxisThreshold,
+		KeyRepeatDelay,
+		KeyRepeatInterval,
+		RailDriverMPH,
+		CursorHideDelay,
+		// Sound
+		Model,
+		Range,
+		Number,
+		// Verbosity
+		ShowWarningMessages,
+		ShowErrorMessages,
+		DebugLog,
+		// Loading
+		ShowLogo,
+		ShowProgressBar,
+		ShowBackground,
+		// Parsers
+		XObject,
+		ObjObject,
+		GDIPlus,
+		// Folders
+		RouteSearch,
+		Route,
+		Train,
+		// Packages
+		Compression,
+		// Touch
+		Cursor,
+		Panel2Extended,
+		Panel2ExtendedMinSize
+
+    }
+}
