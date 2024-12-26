21ba9fa5f83a485487b111ad34d10d71528979d2
Christopher Lees <leezer3@gmail.com>
2024-12-26 20:10:02 +0000

    Start implementing SMS parser

--- source/Plugins/Train.MsTs/Sound/SmsParser.cs
+++ source/Plugins/Train.MsTs/Sound/SmsParser.cs
@@ -1,7 +1,138 @@
-﻿namespace Train.MsTs
+﻿using OpenBve.Formats.MsTs;
+using SharpCompress.Compressors.Deflate;
+using System.IO;
+using System;
+using System.Text;
+using OpenBveApi.Interface;
+using SharpCompress.Compressors;
+using TrainManager.Car;
+
+namespace Train.MsTs
 {
 	class SoundModelSystemParser
 	{
+		private static string currentFolder;
 
+		internal static bool ParseSoundFile(string fileName, ref CarBase Car)
+		{
+			currentFolder = Path.GetDirectoryName(fileName);
+			Stream fb = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+			byte[] buffer = new byte[34];
+			fb.Read(buffer, 0, 2);
+
+			bool unicode = (buffer[0] == 0xFF && buffer[1] == 0xFE);
+
+			string headerString;
+			if (unicode)
+			{
+				fb.Read(buffer, 0, 32);
+				headerString = Encoding.Unicode.GetString(buffer, 0, 16);
+			}
+			else
+			{
+				fb.Read(buffer, 2, 14);
+				headerString = Encoding.ASCII.GetString(buffer, 0, 8);
+			}
+
+			// SIMISA@F  means compressed
+			// SIMISA@@  means uncompressed
+			if (headerString.StartsWith("SIMISA@F"))
+			{
+				fb = new ZlibStream(fb, CompressionMode.Decompress);
+			}
+			else if (headerString.StartsWith("\r\nSIMISA"))
+			{
+				// ie us1rd2l1000r10d.s, we are going to allow this but warn
+				Console.Error.WriteLine("Improper header in " + fileName);
+				fb.Read(buffer, 0, 4);
+			}
+			else if (!headerString.StartsWith("SIMISA@@"))
+			{
+				Plugin.currentHost.AddMessage(MessageType.Error, false, "Unrecognized SMS file header " + headerString + " in " + fileName);
+				return false;
+			}
+
+			string subHeader;
+			if (unicode)
+			{
+				fb.Read(buffer, 0, 32);
+				subHeader = Encoding.Unicode.GetString(buffer, 0, 16);
+			}
+			else
+			{
+				fb.Read(buffer, 0, 16);
+				subHeader = Encoding.ASCII.GetString(buffer, 0, 8);
+			}
+
+			if (subHeader[7] == 't')
+			{
+				using (BinaryReader reader = new BinaryReader(fb))
+				{
+					byte[] newBytes = reader.ReadBytes((int)(fb.Length - fb.Position));
+					string s;
+					if (unicode)
+					{
+						s = Encoding.Unicode.GetString(newBytes);
+					}
+					else
+					{
+						s = Encoding.ASCII.GetString(newBytes);
+					}
+
+					TextualBlock block = new TextualBlock(s, KujuTokenID.Tr_SMS);
+					ParseBlock(block);
+				}
+
+			}
+			else if (subHeader[7] != 'b')
+			{
+				Plugin.currentHost.AddMessage(MessageType.Error, false, "Unrecognized subHeader " + subHeader + " in " + fileName);
+				return false;
+			}
+			else
+			{
+				using (BinaryReader reader = new BinaryReader(fb))
+				{
+					KujuTokenID currentToken = (KujuTokenID)reader.ReadUInt16();
+					if (currentToken != KujuTokenID.Tr_SMS)
+					{
+						return false;
+					}
+
+					reader.ReadUInt16();
+					uint remainingBytes = reader.ReadUInt32();
+					byte[] newBytes = reader.ReadBytes((int)remainingBytes);
+					BinaryBlock block = new BinaryBlock(newBytes, KujuTokenID.Tr_SMS);
+					ParseBlock(block);
+				}
+			}
+
+			return false;
+		}
+
+		private static void ParseBlock(Block block)
+		{
+			Block newBlock;
+			switch (block.Token)
+			{
+				case KujuTokenID.ScalabiltyGroup:
+					/*
+					 * The root container for sound groupings.
+					 * First, parse the activation triggers
+					 */
+					newBlock = block.ReadSubBlock(KujuTokenID.Activation);
+					ParseBlock(newBlock);
+					break;
+				case KujuTokenID.Activation:
+					while (block.Position() < block.Length())
+					{
+						newBlock = block.ReadSubBlock();
+						ParseBlock(newBlock);
+					}
+					break;
+
+			}
+		}
 	}
 }
