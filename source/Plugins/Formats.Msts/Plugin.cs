784b83190549adcf5ce52339addf137b4d4cbbff
Christopher Lees <leezer3@gmail.com>
2025-03-05 09:54:18 +0000

    More WIP

--- source/Plugins/Formats.Msts/Plugin.cs
+++ source/Plugins/Formats.Msts/Plugin.cs
@@ -26,7 +26,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using OpenBveApi.World;
