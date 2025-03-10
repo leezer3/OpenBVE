2bc8d13fee0123f66c8a5090af639ba11a2baacf
Christopher Lees <leezer3@gmail.com>
2025-03-10 10:58:02 +0000

    Change: Generate default rotations for MSTS wheels if not specified
    MSTS does this by default (yuck)

--- source/OpenBveApi/Objects/ObjectTypes/KeyframeAnimatedObject/KeyframeAnimation.cs
+++ source/OpenBveApi/Objects/ObjectTypes/KeyframeAnimatedObject/KeyframeAnimation.cs
@@ -1,4 +1,4 @@
-﻿//Simplified BSD License (BSD-2-Clause)
+//Simplified BSD License (BSD-2-Clause)
 //
 //Copyright (c) 2024, Christopher Lees, The OpenBVE Project
 //
@@ -117,7 +117,21 @@ namespace OpenBveApi.Objects
 					double wheelRadius;
 					if (baseCar.Wheels != null && baseCar.Wheels.ContainsKey(Name))
 					{
-						wheelRadius = baseCar.Wheels[Name].Radius;
+						if (baseCar.Wheels.ContainsKey(Name))
+						{
+							wheelRadius = baseCar.Wheels[Name].Radius;
+						}
+						else
+						{
+							// if controllers are not defined for WHEELSN-N, it appears that those for the base WHEELSN are used
+							// otherwise, controlled by the position of WHEELS1
+							string baseName = Name.Substring(0, 7);
+							if (!baseCar.Wheels.ContainsKey(baseName))
+							{
+								baseName = "WHEELS1";
+							}
+							wheelRadius = baseCar.Wheels[baseName].Radius;
+						}
 					}
 					else
 					{
---
+++ source/Plugins/Object.Msts/WheelRotation.cs
@@ -0,0 +1,22 @@
+﻿using OpenBveApi.Math;
+using OpenBveApi.Objects;
+
+namespace Plugin
+{
+	partial class MsTsShapeParser
+    {
+		/// <summary>Default set of wheel rotation frames</summary>
+	    private static readonly QuaternionFrame[] defaultWheelRotationFrames = new[]
+	    {
+		    new QuaternionFrame(0, new Quaternion(0.707107, 0, 0, -0.707107)),
+		    new QuaternionFrame(1, new Quaternion(0.382683, 0, 0, -0.92388)),
+			new QuaternionFrame(2, new Quaternion(0, 0, 0, -1)),
+			new QuaternionFrame(3, new Quaternion(-0.382683, 0, 0, -0.92388)),
+			new QuaternionFrame(4, new Quaternion(-0.707107, 0, 0, -0.707107)),
+			new QuaternionFrame(5, new Quaternion(-0.923879, 0, 0, -0.382684)),
+			new QuaternionFrame(6, new Quaternion(-1, 0, 0, 0)),
+			new QuaternionFrame(7, new Quaternion(-0.92388, 0, 0, 0.382683)),
+			new QuaternionFrame(8, new Quaternion(0.707107, 0, 0, -0.707106)),
+		};
+    }
+}
