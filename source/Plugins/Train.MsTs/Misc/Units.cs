e780c0e8485ce92e69f0ba709e20e56ebf11e227
Christopher Lees <leezer3@gmail.com>
2025-03-10 13:23:32 +0000

    Hook in bell, horn and headlights
    Only panel simulation for headlights at the minute, need to think about lighting cones and stuff...

---
+++ source/Plugins/Train.MsTs/Misc/Units.cs
@@ -0,0 +1,15 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+namespace Train.MsTs
+{
+    internal enum Units
+    {
+		Unknown = 0,
+		Amps,
+		Inches_Of_Mercury,
+		Kilometers_Per_Hour,
+		Miles_Per_Hour,
+		PSI,
+		Kilo_Lbs
+    }
+}
