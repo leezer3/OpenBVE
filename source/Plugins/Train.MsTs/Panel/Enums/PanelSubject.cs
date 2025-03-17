53435f735c69932360db25fcaf1887a08146713a
Christopher Lees <leezer3@gmail.com>
2025-03-17 17:24:18 +0000

    New: Create simple diesel engine simulation
    Only does RPM and fuel usage for the minute, no physics effects
    WIP: MSTS consist / vehicle parser
    Change: Improve MSTS vehicle search tree
    
    Change: Pull out brake system enums, implement flipped cars
    
    Change: Work on brake systems in MSTS parser
    
    Change: Work on cabviews in MSTS parser
    
    Change: Internal work to get closer to main program loading
    Change: More internal work
    
    Change: Get in-game loading working right
    Change: Get the MSTS train parser loading a description
    
    Change: Cleaning...
    
    Change: Work on cabview loading
    
    Change: Work on getting the thing moving
    
    Change: Partial implementation of some MSTS bits
    Rebase fixing
    Minor fixes for missing properties
    
    Start wheel animation loading
    
    Link unlinked animations to base wheels
    
    Camera restriction should be turned on for CVF
    
    Minor CVF bits
    
    Don't throw exceptions in CVF
    
    Start implementing SMS parser
    
    Separate the handle files into subdirectories
    WIP
    
    Fix allowed empty blocks in MSTS parser
    Start parsing / validating some more SMS bits
    
    More work on SMS parser
    Handle incorrect number of triggers in block
    
    More WIP
    
    Improve wagon type parsing
    
    Add some missing cab component types
    
    More WIP
    
    Validate H/W/L of read wagon
    
    More WIP
    
    Change acceleration calculations to account for uncoupling possibility
    
    Handle current method of MSTS acceleration curve output
    
    Parse basic handle sounds
    
    Alter deceleration curves
    
    Use averages pressure distribution for the minute
    
    Start basic implementation of some more bits
    
    Hook in MSTS wipers
    
    Only cab control works for the minute
    
    Improve wheel radius reading from ENG file
    
    Change: Generate default rotations for MSTS wheels if not specified
    
    MSTS does this by default (yuck)
    
    New: Implement moving bogies for MSTS models

---
+++ source/Plugins/Train.MsTs/Panel/Enums/PanelSubject.cs
@@ -0,0 +1,17 @@
+﻿namespace Train.MsTs
+{
+	internal enum PanelSubject
+	{
+		Speedometer = 0,
+		BrakePipe = 1,
+		BrakeCylinder = 2,
+		Ammeter = 3,
+		Direction = 4,
+		PowerHandle = 5,
+		EngineBrakeHandle = 6,
+		TrainBrakeHandle = 7,
+		Horn = 8,
+		AWS = 9,
+		WiperState = 10,
+	}
+}
