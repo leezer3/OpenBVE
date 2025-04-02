db4ae60c09fc211025b1a7b5bcc65d52462d370c
Christopher Lees <leezer3@gmail.com>
2025-04-02 12:16:11 +0100

    New: Create variable handle type

---
+++ source/TrainManager/Handles/VariableHandle.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using OpenBveApi.Colors;
+using TrainManager.Trains;
+
+namespace TrainManager.Handles
+{
+	public class VariableHandle : AbstractHandle
+	{
+		public VariableHandle(TrainBase train, Tuple<double, string>[] notches = null) : base(train)
+		{
+			if (notches?.Length > 0)
+			{
+				MaximumDriverNotch = notches.Length - 1;
+				MaximumNotch = notches.Length - 1;
+				_notches = notches;
+			}
+			else
+			{
+				MaximumDriverNotch = 100;
+				MaximumNotch = 100;
+			}
+			
+		}
+
+		private readonly Tuple<double, string>[] _notches;
+
+		private double timer;
+
+		private bool isIncreasing;
+
+		
+		public override void Update(double timeElapsed)
+		{
+			if (_notches != null)
+			{
+				return;
+			}
+			timer += timeElapsed;
+			if (ContinuousMovement && timer > 0.1)
+			{
+				if (isIncreasing)
+				{
+					Actual++;
+				}
+				else
+				{
+					Actual--;
+				}
+
+				Actual = Math.Min(100, Math.Max(0, Actual));
+				timer = 0;
+			}
+		}
+
+		public override void ApplySafetyState(int newState)
+		{
+		}
+
+		public override void ApplyState(int newState, bool relativeChange, bool isOverMaxDriverNotch = false)
+		{
+			if (_notches != null)
+			{
+				if (relativeChange)
+				{
+					Actual += newState;
+				}
+				else
+				{
+					Actual = newState;
+				}
+			}
+			else
+			{
+				if (relativeChange)
+				{
+					isIncreasing = newState > 0;
+				}
+			}
+			
+
+			Actual = Math.Min(Actual, MaximumNotch);
+			Driver = Actual;
+		}
+
+		public override string GetNotchDescription(out MessageColor color)
+		{
+			color = Actual == 0 ? MessageColor.Gray : MessageColor.Blue;
+
+			if (_notches == null)
+			{
+				if (Actual == 0)
+				{
+					return "N";
+				}
+				return "P" + Actual + "%";
+			}
+
+			return _notches[Actual].Item2;
+		}
+
+		public double GetPowerModifier
+		{
+			get
+			{
+				if (_notches == null)
+				{
+					return (double)Actual / MaximumDriverNotch;
+				}
+
+				return _notches[Actual].Item1;
+			}
+		}
+	}
+}
--- source/TrainManager/Train/TrainBase.cs
+++ source/TrainManager/Train/TrainBase.cs
@@ -479,9 +479,9 @@ namespace TrainManager.Trains
 				Handles.EmergencyBrake.Safety = Handles.EmergencyBrake.Driver;
 			}
 
-			Handles.Power.Update();
-			Handles.Brake.Update();
-			Handles.Brake.Update();
+			Handles.Power.Update(TimeElapsed);
+			Handles.Brake.Update(TimeElapsed);
+			Handles.Brake.Update(TimeElapsed);
 			Handles.EmergencyBrake.Update();
 			Handles.HoldBrake.Actual = Handles.HoldBrake.Driver;
 			Cars[DriverCar].DSD?.Update(TimeElapsed);
