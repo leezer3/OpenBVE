0a82b739d0358b397f28ff1e0107fb2650bcf364
Christopher Lees <leezer3@gmail.com>
2025-04-16 10:00:00 +0100

    Start to implement SoundStreams

--- source/OpenBveApi/World/UnitConversions/Velocity.cs
+++ source/OpenBveApi/World/UnitConversions/Velocity.cs
@@ -23,7 +23,7 @@ namespace OpenBveApi.World
 			RegisterConversion(UnitOfVelocity.MilesPerHour, v => v * 2.237, v => v / 2.237);
 			KnownUnits = new Dictionary<string, UnitOfVelocity>
 			{
-				{"m/s", UnitOfVelocity.MetersPerSecond}, {"mph", UnitOfVelocity.MilesPerHour}, {"kmh", UnitOfVelocity.KilometersPerHour}, {"km/h", UnitOfVelocity.KilometersPerHour}
+				{"m/s", UnitOfVelocity.MetersPerSecond}, {"mph", UnitOfVelocity.MilesPerHour}, {"kmh", UnitOfVelocity.KilometersPerHour}, {"km/h", UnitOfVelocity.KilometersPerHour}, {"meterspersecond", UnitOfVelocity.MetersPerSecond}
 			};
 		}
 
