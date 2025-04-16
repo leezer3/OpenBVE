78deab712f45650eb3cb7c39f1f2f9d1dd34bf57
Christopher Lees <leezer3@gmail.com>
2025-04-16 10:00:00 +0100

    Start to implement SoundStreams

---
+++ source/TrainManager/Sounds/MSTS/SoundTrigger.cs
@@ -0,0 +1,25 @@
+﻿using SoundManager;
+using TrainManager.Car;
+
+namespace TrainManager.MsTsSounds
+{
+	public abstract class SoundTrigger
+	{
+		internal readonly SoundBuffer Buffer;
+
+		internal SoundSource Source;
+
+		internal readonly CarBase Car;
+
+		internal SoundTrigger(CarBase car, SoundBuffer buffer)
+		{
+			Buffer = buffer;
+			Car = car;
+		}
+
+		public virtual void Update(double timeElapsed, double pitchValue, double volumeValue)
+		{
+
+		}
+	}
+}
