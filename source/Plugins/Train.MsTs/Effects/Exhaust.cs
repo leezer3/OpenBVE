468aa72670851fa03a3a5f30e074bc27ed24f511
Christopher Lees <leezer3@gmail.com>
2025-04-20 19:30:25 +0100

    Procedural smoke generator
    Fix issues from rebasing to current master
    
    Change matrix buffer creation
    
    Something broken somewhere, but it seems to work this way....

---
+++ source/Plugins/Train.MsTs/Effects/Exhaust.cs
@@ -0,0 +1,19 @@
+﻿using OpenBveApi.Math;
+
+namespace Train.MsTs
+{
+	internal struct Exhaust
+	{
+		internal Vector3 Offset;
+
+		internal Vector3 Direction;
+
+		internal double Size;
+
+		internal double SmokeMaxMagnitude;
+
+		internal double SmokeInitialRate;
+
+		internal double SmokeMaxRate;
+	}
+}
