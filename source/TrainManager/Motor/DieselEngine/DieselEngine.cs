bc82f1f5e31791dddd726f0b77e48a8c6ca554c7
Christopher Lees <leezer3@gmail.com>
2025-07-21 13:59:57 +0100

    Add some traction debug overlay messages

--- source/TrainManager/Motor/DieselEngine/DieselEngine.cs
+++ source/TrainManager/Motor/DieselEngine/DieselEngine.cs
@@ -101,15 +101,19 @@ namespace TrainManager.Motor
 				CurrentRPM = Math.Max(CurrentRPM, targetRPM);
 			}
 
+			Message = @"Current RPM " + Math.Round(CurrentRPM);
+
 			if (FuelTank != null)
 			{
 				if (currentRPM <= IdleRPM)
 				{
 					FuelTank.CurrentLevel -= IdleFuelUse * timeElapsed;
+					Message += " Fuel Use " + Math.Round(IdleFuelUse * timeElapsed, 5);
 				}
 				else
 				{
 					FuelTank.CurrentLevel -= (MaxPowerFuelUse - IdleFuelUse) / BaseCar.baseTrain.Handles.Power.MaximumDriverNotch * BaseCar.baseTrain.Handles.Power.Actual * timeElapsed;
+					Message += " Fuel Use " + Math.Round((MaxPowerFuelUse - IdleFuelUse) / BaseCar.baseTrain.Handles.Power.MaximumDriverNotch * BaseCar.baseTrain.Handles.Power.Actual * timeElapsed, 5);
 				}
 			}
 
