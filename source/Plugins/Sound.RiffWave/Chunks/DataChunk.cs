8264e4b9273678a79f71394d1eb27b10014af58f
Christopher Lees <leezer3@gmail.com>
2025-07-21 16:43:25 +0100

    New: Attempt to support WAV files with multiple DATA / SLNT chunks
    Needs testing

---
+++ source/Plugins/Sound.RiffWave/Chunks/DataChunk.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+
+namespace Plugin
+{
+    internal class DataChunk : Chunk
+    {
+	    public DataChunk(byte[] dataBytes, WaveFormatEx format) : base(format.Channels)
+	    {
+		    int bytesPerSample;
+
+		    if (!(format is WaveFormatAdPcm))
+		    {
+			    bytesPerSample = format.BitsPerSample / 8;
+		    }
+		    else
+		    {
+			    bytesPerSample = 2;
+		    }
+
+		    // The size of the data chunk may not be correct, so we use sampleCount as the standard thereafter.
+		    int sampleCount = dataBytes.Length / (format.Channels * bytesPerSample);
+
+		    byte[][] buffers = new byte[format.Channels][];
+
+		    for (int i = 0; i < format.Channels; i++)
+		    {
+			    buffers[i] = new byte[sampleCount * bytesPerSample];
+		    }
+
+		    for (int i = 0; i < sampleCount; i++)
+		    {
+			    for (int j = 0; j < format.Channels; j++)
+			    {
+				    for (int k = 0; k < bytesPerSample; k++)
+				    {
+					    buffers[j][i * bytesPerSample + k] = dataBytes[i * bytesPerSample * format.Channels + bytesPerSample * j + k];
+				    }
+			    }
+		    }
+
+		    Buffers = ChangeBitDepth(format.FormatTag, bytesPerSample, sampleCount, buffers);
+	    }
+
+		private static byte[][] ChangeBitDepth(WFormatTag formatTag, int bytesPerSample, int sampleCount, byte[][] buffers)
+		{
+			byte[][] newBuffers = new byte[buffers.Length][];
+
+			for (int i = 0; i < buffers.Length; i++)
+			{
+				newBuffers[i] = new byte[sampleCount * sizeof(short)];
+			}
+
+			switch (formatTag)
+			{
+				case WFormatTag.Extensible:
+				case WFormatTag.PCM:
+					switch (bytesPerSample)
+					{
+						case 3:
+							for (int i = 0; i < buffers.Length; i++)
+							{
+								using (var stream = new MemoryStream(newBuffers[i]))
+								using (var writer = new BinaryWriter(stream))
+								{
+									for (int j = 0; j < buffers[i].Length; j += 3)
+									{
+										byte[] tmp = new byte[4];
+										Array.Copy(buffers[i], j, tmp, 0, 3);
+										tmp[3] = (byte)(tmp[2] > 0x7F ? 0xff : 0x00);
+										float sample = BitConverter.ToInt32(tmp, 0) / 8388608.0f;
+										writer.Write((short)(sample * short.MaxValue));
+									}
+								}
+							}
+
+							bytesPerSample = sizeof(short);
+							break;
+						case 4:
+							for (int i = 0; i < buffers.Length; i++)
+							{
+								using (var stream = new MemoryStream(newBuffers[i]))
+								using (var writer = new BinaryWriter(stream))
+								{
+									for (int j = 0; j < buffers[i].Length; j += 4)
+									{
+										float sample = BitConverter.ToInt32(buffers[i], j) / 2147483648.0f;
+										writer.Write((short)(sample * short.MaxValue));
+									}
+								}
+							}
+
+							bytesPerSample = sizeof(short);
+							break;
+						default:
+							newBuffers = buffers;
+							break;
+					}
+					break;
+				case WFormatTag.IEEE_Float:
+					for (int i = 0; i < buffers.Length; i++)
+					{
+						using (var stream = new MemoryStream(newBuffers[i]))
+						using (var writer = new BinaryWriter(stream))
+						{
+							for (int j = 0; j < buffers[i].Length; j += 4)
+							{
+								float sample = BitConverter.ToSingle(buffers[i], j);
+
+								if (sample < -1.0f)
+								{
+									sample = -1.0f;
+								}
+
+								if (sample > 1.0f)
+								{
+									sample = 1.0f;
+								}
+
+								writer.Write((short)(sample * short.MaxValue));
+							}
+						}
+					}
+
+					bytesPerSample = sizeof(short);
+					break;
+				default:
+					newBuffers = buffers;
+					break;
+			}
+
+			return newBuffers;
+		}
+	}
+}
