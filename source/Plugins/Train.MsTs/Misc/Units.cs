429f29a82b795e10583263f8c97a3eae11df24d3
Christopher Lees <leezer3@gmail.com>
2025-09-17 14:10:29 +0100

    Add some missing values

--- source/Plugins/Train.MsTs/Misc/Units.cs
+++ source/Plugins/Train.MsTs/Misc/Units.cs
@@ -6,12 +6,15 @@ namespace Train.MsTs
     internal enum Units
     {
 		Unknown = 0,
-		Amps,
-		Gallons,
-		Inches_Of_Mercury,
-		Kilometers_Per_Hour,
-		Miles_Per_Hour,
-		PSI,
-		Kilo_Lbs
+		Amps =1,
+		Gallons = 2,
+		Inches_Of_Mercury = 3,
+		Kilometers_Per_Hour = 4,
+		Km_Per_Hour = 4,
+		Miles_Per_Hour = 5,
+		Meters_Sec = 6,
+		PSI = 7,
+		Kilo_Lbs = 8,
+		Kilopascals = 9
     }
 }
