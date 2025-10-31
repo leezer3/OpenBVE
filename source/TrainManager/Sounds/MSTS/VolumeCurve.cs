585c088eb373d7340959109cc7bb0fc082891f31
Christopher Lees <leezer3@gmail.com>
2025-10-31 16:36:20 +0000

    Start to implement SoundStreams

---
+++ source/TrainManager/Sounds/MSTS/VolumeCurve.cs
@@ -0,0 +1,50 @@
+﻿using OpenBve.Formats.MsTs;
+using System;
+using TrainManager.Car;
+
+namespace TrainManager.MsTsSounds
+{
+	public class MsTsVolumeCurve
+	{
+		private readonly CarBase car;
+		private readonly Tuple<double, double>[] volumePoints;
+
+		private readonly KujuTokenID controller;
+
+		public MsTsVolumeCurve(CarBase car, KujuTokenID controller, Tuple<double, double>[] points)
+		{
+			this.car = car  as CarBase;
+			this.controller = controller;
+			volumePoints = points;
+		}
+
+		public double Volume
+		{
+			get
+			{
+				switch (controller)
+				{
+					case KujuTokenID.SpeedControlled:
+						for (int i = volumePoints.Length - 1; i >= 0; i--)
+						{
+							if (Math.Abs(car.CurrentSpeed) >= volumePoints[i].Item1)
+							{
+								return volumePoints[i].Item2;
+							}
+						}
+						break;
+					case KujuTokenID.Variable2Controlled:
+						for (int i = volumePoints.Length - 1; i >= 0; i--)
+						{
+							if (car.TractionModel.CurrentPower >= volumePoints[i].Item1)
+							{
+								return volumePoints[i].Item2;
+							}
+						}
+						break;
+				}
+				return 0;
+			}
+		}
+	}
+}
