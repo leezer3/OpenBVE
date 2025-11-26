892e51ea4fa57fef83538631bbdd607c62693b2b
Christopher Lees <leezer3@gmail.com>
2025-11-26 16:29:42 +0000

    Change: Use enum in TFO parser, shift to consistant camelCase

---
+++ source/Plugins/Formats.OpenBve/Enums/TrackFollowingObjectXML/TrackFollowingObject.cs
@@ -0,0 +1,31 @@
+﻿namespace Formats.OpenBve
+{
+	public enum TrackFollowingObjectKey
+	{
+		Unknown = 0,
+		Definition,
+		RunInterval,
+		PreTrain,
+		Train,
+		Points,
+		Stops,
+		AppearanceTime,
+		AppearanceStartPosition,
+		AppearanceEndPosition,
+		LeaveTime,
+		Directory,
+		Reversed,
+		Stop,
+		Point,
+		Decelerate,
+		Position,
+		StopPosition,
+		Accelerate,
+		TargetSpeed,
+		Rail,
+		StopTime,
+		Doors,
+		Direction,
+		PassingSpeed,
+	}
+}
