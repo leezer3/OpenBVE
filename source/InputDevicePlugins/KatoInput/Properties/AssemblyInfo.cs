f5a22eef718896f6a3bdb20e01ee4a9e883a2136
Marc Riera <marc@marcriera.cat>
2026-03-14 11:27:58 +0100

    Add KATO input plugin

---
+++ source/InputDevicePlugins/KatoInput/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("KATO controller plugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KatoInput")]
+[assembly: AssemblyCopyright("Marc Riera, The OpenBVE Project")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
