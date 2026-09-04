fcc15a731fb6355d5682cf25153162b7bbcf6f4b
Christopher Lees <leezer3@gmail.com>
2026-09-04 15:33:54 +0100

    Change: Add TriFace, TriFace2 to CSV B3D parser

--- source/Plugins/Formats.OpenBve/Enums/CSVB3D/CSVB3DKey.cs
+++ source/Plugins/Formats.OpenBve/Enums/CSVB3D/CSVB3DKey.cs
@@ -12,9 +12,13 @@ namespace Formats.OpenBve
 		/// <summary>Adds a new 1-sided face</summary>
 		Face,
 		AddFace = Face,
+		/// <summary>Adds a new 1-sided face consisting of triangles</summary>
+		TriFace,
 		/// <summary>Adds a new 2-sided face</summary>
 		Face2,
 		AddFace2 = Face2,
+		/// <summary>Adds a new 2-sided face consisting of triangles</summary>
+		TriFace2,
 		/// <summary>Sets the color of all preceding faces in the MeshBuilder</summary>
 		Color,
 		SetColor = Color,
